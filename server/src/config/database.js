abf021d753b9bec774da9b12e1127ffca0e97856
Atirson Fabiano Barbosa de Oliveira <atirson.oliveira@squadra.com.br>
2020-05-29 14:17:02 -0300

    chore: Trying to make deploy.

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -2,10 +2,6 @@ require('dotenv/config');
 
 module.exports = {
   dialect: 'sqlite',
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
   storage: './src/database/database.sqlite',
   define: {
     timestamps: true,
